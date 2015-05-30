ba44ceb113e2142c62317360fe514219fe0591b2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-30 16:28:21 -0600

    basic lexer almost working

--- src/jaxe/core/Lexer.hx
+++ src/jaxe/core/Lexer.hx
@@ -9,6 +9,7 @@ class Lexer {
 	var lineNumber : Int;
 	var ended : Bool;
 	var tokens : Array<Token>;
+	var indentStack : Array<Int>;
 
 	public function new(content : String, source : String) {
 		input = content;
@@ -20,6 +21,8 @@ class Lexer {
 		this.source = source;
 		lastIndents = 0;
 		lineNumber = 1;
+		tokens = [];
+		indentStack = [];
 	}
 
 	public function getTokens() {
@@ -31,14 +34,45 @@ class Lexer {
 	function blank()
 		return false;
 
-	function eos()
-		return false;
+	function doctype()
+		return scan(~/^doctype +([^\n]+)?/, function(reg) {
+			return TDoctype(switch reg.matched(1) {
+				case "html": HtmlDoctype;
+				case "xml": XmlDoctype;
+				case "transitional": XhtmlTransitionalDoctype;
+				case "strict": XhtmlStrictDoctype;
+				case "frameset": XhtmlFramesetDoctype;
+				case "1.1": Xhtml11Doctype;
+				case "basic": BasicDoctype;
+				case "mobile": MobileDoctype;
+				case custom: CustomDoctype(custom);
+			});
+		});
+
+	function eos() {
+		if(input.length > 0) return false;
+		for(i in 0...indentStack.length)
+			tokens.push(TOutdent);
+		tokens.push(TEos);
+		ended = true;
+		return true;
+	}
 
 	function fail() : Bool
 		return throw new LexerError('unexpected text: ${input.split("\n").shift()}');
 
+	function scan(reg : EReg, f : EReg -> Null<Token>) : Bool {
+		if(!reg.match(input)) return false;
+		var v = f(reg);
+		if(null != v)
+			tokens.push(v);
+		input = reg.matchedRight();
+		return true;
+	}
+
 	function advance()
 		return blank()
 			|| eos()
+			|| doctype()
 			|| fail();
 }
