e86163c99f4e31dfd278715743d0bcaf8d3f28af
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-30 11:06:09 -0600

    cleanup

--- src/jaxe/core/Indent.hx
+++
@@ -1,6 +0,0 @@
-package jaxe.core;
-
-enum Indent {
-  WS(spaces : Int);
-  Tabs(tabs : Int);
-}
--- test/TestLexer.hx
+++ test/TestLexer.hx
@@ -3,23 +3,8 @@ import utest.Assert;
 using thx.Arrays;
 import jaxe.core.Lexer;
 import jaxe.core.Token;
-import BaseLexer.*;
 
-class TestLexer extends BaseLexer {
-/*
-	public function testDoctypes() {
-		[
-			{ _0 : "doctype html", _1 : [HtmlDoctype], _2 : here() },
-			{ _0 : "doctype xml", _1 : [XmlDoctype], _2 : here() },
-			{ _0 : "doctype transitional", _1 : [XhtmlTransitionalDoctype], _2 : here() },
-			{ _0 : "doctype strict", _1 : [XhtmlStrictDoctype], _2 : here() },
-			{ _0 : "doctype frameset", _1 : [XhtmlFramesetDoctype], _2 : here() },
-			{ _0 : "doctype 1.1", _1 : [Xhtml11Doctype], _2 : here() },
-			{ _0 : "doctype basic", _1 : [BasicDoctype], _2 : here() },
-			{ _0 : "doctype mobile", _1 : [MobileDoctype], _2 : here() },
-			{ _0 : "doctype html PUBLIC \"-//W3C//DTD XHTML Basic 1.1//EN\"", _1 : [CustomDoctype("html PUBLIC \"-//W3C//DTD XHTML Basic 1.1//EN\"")], _2 : here() },
-			{ _0 : "some random text", _1 : [], _2 : here() },
-		].pluck(assertLexer(Lexer.doctype, _._1, _._0, _._2));
-	}
-*/
+class TestLexer {
+	public function new() {}
+
 }
