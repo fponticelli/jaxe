23c2a5d71170cc722abb47c3a9d315e2069170b7
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-29 21:49:53 -0600

    fixed BaseLexer

--- test/BaseLexer.hx
+++ test/BaseLexer.hx
@@ -2,25 +2,29 @@ import utest.Assert;
 
 import jaxe.core.Lexer;
 import jaxe.core.Token;
+import hxparse.Ruleset;
 
 class BaseLexer {
 	public function new() { }
 
-	function assertLexer(ruleset, expected : Array<ValueToken>, test : String, ?pos : haxe.PosInfos) {
+	function assertLexer<T>(ruleset : Ruleset<T>, expected : Array<T>, test : String, ?pos : haxe.PosInfos) {
 		var data = byte.ByteData.ofString(test);
 		var lexer = new Lexer(data, '${pos.className}.${pos.methodName}');
 		var tokens = [];
 		try while (true) {
 			var t = lexer.token(ruleset);
 			tokens.push(t);
-		} catch (e:Dynamic) { }
+		} catch (e:Dynamic) {
+			trace(e);
+		}
 		Assert.equals(expected.length, tokens.length, "number of expressions do not match for $expected and $tests", pos);
 		for(i in 0...expected.length) {
-			assertSameValueToken(expected[i], tokens[i], pos);
+			Assert.same(expected[i], tokens[i], pos);
 		}
 	}
 
-	function assertSameValueToken(a, b, ?pos : haxe.PosInfos) {
+/*
+	function assertSameValueToken<T>(a : T, b : T, ?pos : haxe.PosInfos) {
 		switch [a, b] {
 			case [TString(sa, _), TString(sb, _)]:
 				Assert.equals(sa, sb, pos);
@@ -30,6 +34,6 @@ class BaseLexer {
 				Assert.fail('$a doesn\'t match $b', pos);
 		}
 	}
-
+*/
 	public inline static function here(?pos : haxe.PosInfos) return pos;
 }
