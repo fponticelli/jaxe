b6bf6ac6348cd16b80db9a8c9ce03e62bb70c98d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-03 23:26:14 -0600

    basic Parser structure

--- src/jaxe/core/Parser.hx
+++ src/jaxe/core/Parser.hx
@@ -1,6 +1,8 @@
 package jaxe.core;
 
 import tokens.Tokens as T;
+import jaxe.core.Node;
+import jaxe.core.Token;
 
 @:access(jaxe.core.Lexer)
 class Parser {
@@ -11,12 +13,31 @@ class Parser {
     this.source = source;
   }
 
-  function error(message : String, ?pos : haxe.PosInfos) {
-    throw new ParserError(message, pos);
+  public function parse() {
+    var block = new Block([], 0, source),
+        next;
+    while(true) {
+      next = peek();
+      switch next.token {
+        case TEos: break;
+        case TNewline: advance();
+        case other: error('Not Implemented Yet: other');
+      }
+    }
+
+    return block;
   }
 
   // utility
-  function peek() {
+  inline function advance()
+    return tokens.advance();
 
-  }
+  function error(message : String, ?pos : haxe.PosInfos)
+    throw new ParserError(message, pos);
+
+  inline function peek()
+    return tokens.peek();
+
+  inline function lookahead(n : Int)
+    return tokens.lookahead(n);
 }
