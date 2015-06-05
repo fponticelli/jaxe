f24b800c8aff937f96833481a2a9c507f733ea85
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-04 23:35:40 -0600

    added Nodes helper class

---
+++ src/jaxe/core/Nodes.hx
@@ -0,0 +1,32 @@
+package jaxe.core;
+
+import jaxe.core.Node;
+
+class Nodes {
+	public static function toObject(node : Node) : NodeObject {
+		return node.toObject();
+	}
+
+	public static function fromObject(obj : NodeObject) : Node {
+		trace(obj.type);
+		trace(obj.pos);
+		return switch obj.type {
+      case "block": Block.fromObject(obj);
+			case "tag": Tag.fromObject(obj);
+			case "text": Text.fromObject(obj);
+			case _: throw new ParserParseError('unknown type "${obj.type}"');
+    };
+	}
+}
+
+typedef NodeObject = {
+  type : String,
+  pos : {
+    line : Int,
+    source : String
+  },
+	?nodes : Array<NodeObject>,
+	?name : String,
+	?content : String,
+	?selfClosing : Bool
+}
